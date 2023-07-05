0cc5bd0d7c1214f6ccdc48e8e7a4dd1dd5c7ec0c
ryoonago <133100923+ryoonago@users.noreply.github.com>
2023-07-05 14:42:12 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> 이것이 CI/CD고 GITACTION이다
         </p>
         <a
           className="App-link"
